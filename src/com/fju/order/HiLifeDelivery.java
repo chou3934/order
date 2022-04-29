d5765e42a4ebe952880a39879b1515192ec7397c
chou3934 <choumelanie12@gmail.com>
2022-04-29 11:58:44 +0800

    1st commit

---
+++ src/com/fju/order/HiLifeDelivery.java
@@ -0,0 +1,8 @@
+package com.fju.order;
+
+public class HiLifeDelivery extends FamilyDelivery{
+    public HiLifeDelivery(){
+        name="Hi-Life";
+        price=50;
+    }
+}
---
+++ src/com/fju/order/ShopeeDelivery.java
@@ -0,0 +1,8 @@
+package com.fju.order;
+
+public class ShopeeDelivery extends HiLifeDelivery{
+    public ShopeeDelivery(){
+        name="Shopee" ;
+        price=19;
+    }
+}
